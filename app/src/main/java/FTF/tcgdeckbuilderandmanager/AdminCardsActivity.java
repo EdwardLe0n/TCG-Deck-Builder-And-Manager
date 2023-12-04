8d38af74d662cf60c62c74ae8d927b8f9b5ff06b
Edward Leon <edleon761@gmail.com>
2023-12-03 21:39:18 -0800

    Just brought in the two new activities. Here's a checkpoint

---
+++ app/src/main/java/FTF/tcgdeckbuilderandmanager/AdminCardsActivity.java
@@ -0,0 +1,14 @@
+package FTF.tcgdeckbuilderandmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminCardsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_cards);
+    }
+}
---
+++ app/src/main/java/FTF/tcgdeckbuilderandmanager/NormieCardsActivity.java
@@ -0,0 +1,14 @@
+package FTF.tcgdeckbuilderandmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NormieCardsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_normie_cards);
+    }
+}
